a25e9272acb1df1e718ceac19677e991cc98c1f6
Jiacheng Hao <2782881499@qq.com>
2026-05-11 23:39:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export defaul defineConfig({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: './',
 })
